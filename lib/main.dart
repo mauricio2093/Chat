a71737d14eacb213b7751f971279180ebc784205
mauricio2093 <mauriciocadena2093@gmail.com>
2023-07-19 23:46:39 -0500

    01-Chat flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:application_2/assets/chat/chat_screen.dart';
+import 'package:application_2/presentation/chat/chat_screen.dart';
 import 'package:application_2/config/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://i.pinimg.com/564x/93/fd/a3/93fda3c4e69aa32beac71721299c6abe.jpg'),
+          ),
+        ),
+        title: const Text('Avatar Receiver'),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Text('Indice : $index');
+                },
+              ),
+            ),
+            Text('Hola'),
+          ],
+        ),
+      ),
+    );
+  }
+}
