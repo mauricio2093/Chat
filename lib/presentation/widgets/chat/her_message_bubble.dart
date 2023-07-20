1c5d5736c93a3225c69da22a17d419f3b6127f03
mauricio2093 <mauriciocadena2093@gmail.com>
2023-07-20 01:06:38 -0500

    02-Chat flutter

---
+++ lib/presentation/widgets/chat/her_message_bubble.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HerMessageBubble extends StatelessWidget {
+  const HerMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: colors.secondary,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Sunt aliquip ex qui et ex reprehenderit officia ipsum et exercitation ea.',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        _ImageBubble(),
+        const SizedBox(
+          height: 10,
+        )
+      ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: Image.network(
+        'https://yesno.wtf/assets/no/21-05540164de4e3229609f106e468fa8e7.gif',
+        width: size.width * 0.7,
+        height: 150,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
